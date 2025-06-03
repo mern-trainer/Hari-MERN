177d4eb1f8b1f94368871a4bb4c5842840004d19
VISHNU M K <futuralabs.mern@gmail.com>
2025-06-03 20:21:49 +0530

    react introduction

---
+++ ReactJS/app/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => { // component
+    return <div>
+        <div>
+            <h1>Hello World</h1>
+            <p>Hello World</p>
+            <ul>
+                <li>Hello World</li>
+                <li>Hello World</li>
+                <li>Hello World</li>
+            </ul>
+        </div>
+        <div>Hello</div>
+    </div>
+}
+
+export default App;
