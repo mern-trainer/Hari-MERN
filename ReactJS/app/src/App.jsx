3ac045fad3f59c6a48db3a7295f650e44df2d762
VISHNU M K <futuralabs.mern@gmail.com>
2025-06-04 20:01:50 +0530

    jsx

--- ReactJS/app/src/App.jsx
+++ ReactJS/app/src/App.jsx
@@ -1,16 +1,40 @@
+import { Fragment } from "react"
+
 const App = () => { // component
-    return <div>
+
+    const age = 30;
+    const city = "Bangalore";
+
+    const handleClick = () => {
+        console.log("Button Clicked")
+    } // function
+
+    const handleClickWithArg = (arg) => {
+        console.log("Button Clicked with arg", arg)
+    } // function
+
+    const generateRandomPassword = () => {
+        const string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+        let password = "";
+        for (let i = 0; i < 16; i++){
+            const randomIndex = Math.floor(Math.random() * string.length);
+            password += string[randomIndex];
+        }
+        document.getElementById("pass").innerHTML = password;
+        document.getElementById("history").innerHTML += password + "<br/>"
+    }
+
+    return <Fragment>
+        {/* <div>{age}</div>
+        <div>City: {city}</div>
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={() => handleClickWithArg(10)}>Click Me - Arg</button> */}
         <div>
-            <h1>Hello World</h1>
-            <p>Hello World</p>
-            <ul>
-                <li>Hello World</li>
-                <li>Hello World</li>
-                <li>Hello World</li>
-            </ul>
+            <div id="pass"></div>
+            <button onClick={generateRandomPassword}>Generate Password</button>
+            <div id="history"></div>
         </div>
-        <div>Hello</div>
-    </div>
+    </Fragment>
 }
 
 export default App;
