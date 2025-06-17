8c1e9a00285e78fe5bba8fb8c4a50b790f031a57
VISHNU M K <futuralabs.mern@gmail.com>
2025-06-17 20:09:47 +0530

    todo list completed

--- ReactJS/app/src/main.jsx
+++ ReactJS/app/src/main.jsx
@@ -2,9 +2,14 @@ import { createRoot } from "react-dom/client"
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./index.css"
+import { Fragment } from "react";
+import { ToastContainer } from "react-toastify";
 
 const container = document.getElementById("root");
 
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(<Fragment>
+    <App />
+    <ToastContainer autoClose={1000} />
+</Fragment>)
--- ReactJS/app/src/pages/TodoList.jsx
+++ ReactJS/app/src/pages/TodoList.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react"
+import { Fragment, useState } from "react"
+import { toast } from "react-toastify"
+import { CiCircleCheck } from "react-icons/ci";
 
 const TodoList = () => {
 
     const [todo, setTodo] = useState("")
     const [todoList, setTodoList] = useState([])
+    const [editableTask, setEditableTask] = useState(null)
+    const [editTask, setEditTask] = useState("")
 
     const handleChange = (event) => {
         setTodo(event.target.value)
@@ -12,11 +16,11 @@ const TodoList = () => {
     const handleSubmit = (event) => {
         event.preventDefault()
         if (todo == "") {
-            return console.log("Task is required.")
+            return toast.error("Task is required.")
         }
         const index = todoList.findIndex((item) => item.task.toLowerCase() == todo.toLowerCase())
         if (index != -1) {
-            return console.log("Task already exist.")
+            return toast.error("Task already exist.")
         }
         const taskObj = {
             id: crypto.randomUUID(),
@@ -27,16 +31,36 @@ const TodoList = () => {
         }
         setTodoList([taskObj, ...todoList])
         setTodo("")
+        return toast.success("Task added successfully")
     }
 
-    const mappedList = todoList.map(listItem => {
-        // identifier (key) -> Which element is created, updated or deleted
-        return <div key={listItem.id} className="border border-2 p-2">
-            <h2>{listItem.task}</h2>
-            <p>Created: {listItem.createdAt}</p>
-            <p>Updated: {listItem.updatedAt}</p>
-        </div>
-    })
+    const handleDelete = id => {
+        const res = todoList.filter(listItem => listItem.id != id)
+        setTodoList(res)
+        return toast.success("Task removed.")
+    }
+
+    const handleTodoUpdate = (id) => {
+        const res = todoList.map(item => {
+            if (item.id == id) {
+                return { ...item, task: editTask, updatedAt: new Date().toLocaleString() }
+            }
+            return item
+        })
+        setTodoList(res)
+        setEditableTask(null)
+        setEditTask("")
+    }
+
+    const handleStatusUpdate = id => {
+        const res = todoList.map(item => {
+            if (item.id == id) {
+                return { ...item, is_completed: true, updatedAt: new Date().toLocaleString() }
+            }
+            return item
+        })
+        setTodoList(res)
+    }
 
     return <div className="d-flex flex-column align-items-center justify-content-center">
         <form onSubmit={handleSubmit} className="w-100 d-flex gap-2 flex-column mt-2" style={{maxWidth: "500px"}}>
@@ -45,7 +69,40 @@ const TodoList = () => {
             <button className="btn btn-secondary rounded-0">Add Task</button>
         </form>
         <div className="w-100 mt-2 d-flex flex-column gap-2 " style={{ maxWidth: "500px" }}>
-            {mappedList}
+            {
+                todoList.length > 0 && todoList.map(listItem => {
+                    return <div key={listItem.id} className="border border-2 p-2">
+                        {
+                            editableTask?.id == listItem.id ? <input type="text" placeholder="Eg: Buy Something..." value={editTask} onChange={e => setEditTask(e.target.value)}/> : <h4>{listItem.task}</h4>
+                        }
+                        <p>Created: {listItem.createdAt}</p>
+                        <p>Updated: {listItem.updatedAt}</p>
+                        <p>Status: {listItem.is_completed ? "Completed" : <span>Pending <CiCircleCheck color="green" cursor={"pointer"} onClick={() => handleStatusUpdate(listItem.id)}/></span>} </p>
+                        <div className="d-flex gap-2">
+                            
+                            {
+                                editableTask?.id == listItem.id ?
+                                    <Fragment>
+                                        <button className="btn btn-primary w-100" onClick={() => handleTodoUpdate(listItem.id)}>Submit</button>
+                                        <button className="btn btn-danger w-100" onClick={() => setEditableTask(null)}>Cancel</button>
+                                    </Fragment>
+                                    : <Fragment>
+                                        {
+                                            !listItem.is_completed && <button className="btn btn-primary w-100" onClick={() => {
+                                                setEditableTask(listItem);
+                                                setEditTask(listItem.task);
+                                            }}>Edit Task</button>
+                                        }
+                                        <button className="btn btn-danger w-100" onClick={() => handleDelete(listItem.id)}>Delete</button>
+                                    </Fragment>
+                            }
+                        </div>
+                    </div>
+                })
+            }
+            {
+                todoList.length == 0 && <div>Task is empty.</div>
+            }
         </div>
     </div>
 }
